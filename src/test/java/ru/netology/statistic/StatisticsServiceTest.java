b6453c67198c76e00eb094c3bce78493d6733fd5
mikhalev valera <mihalev.valera@mail.ru>
2024-09-02 22:11:57 +0300

    add test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -22,8 +22,8 @@ public class StatisticsServiceTest {
     void firstNoMax() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {11, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
+        long[] incomesInBillions = {11, 5, 8, 4, 5, 3, 8, 6, 11, 11, 13};
+        long expected = 13;
 
         long actual = service.findMax(incomesInBillions);
 
